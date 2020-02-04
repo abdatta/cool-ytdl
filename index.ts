0a2a1338d3736f74f85507dda1058f3637367098
Abhishek Datta <abdatta@iitk.ac.in>
2020-02-04 15:42:20 +0530

    Add endpoint for merge and pipe pure video and audio channels

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import * as express from 'express';
 import * as hbs from 'hbs';
 import * as ytdl from 'ytdl-core';
+import { spawn } from 'child_process';
 
 const app = express()
 app.set('view engine', 'hbs');
@@ -28,7 +29,31 @@ app.get('/options', (req, res) => {
 app.get('/download', (req, res) => {
     const link = req.query.link;
     const itag = req.query.itag;
+    if (!(link && itag)) {
+        res.sendStatus(400); // Bad Request;
+        return;
+    }
     ytdl(link, {quality: itag}).pipe(res);
 });
+
+app.get('/merge', (req, res) => {
+    const link = req.query.link;
+    const audio_itag = req.query.a_itag;
+    const video_itag = req.query.v_itag;
+
+    if (!(link && audio_itag && video_itag)) {
+        res.sendStatus(400); // Bad Request;
+        return;
+    }
+
+    const options = ["-i",`http://localhost:4000/download?link=${encodeURIComponent(link)}&itag=${video_itag}`,"-i",`http://localhost:4000/download?link=${encodeURIComponent(link)}&itag=${audio_itag}`,"-map","0","-map","1","-c","copy","-f", "matroska", "pipe:1"];
+    const output = spawn('ffmpeg', options);
+    console.log('\nffmpeg', ...options, '\n');
+
+    output.stderr.pipe(process.stdout);
+    output.stdout.pipe(res);
+
+    output.on('exit', (code) => console.log(`ffmpeg exited with code ${code}\n`));
+});
  
-app.listen(4000)
+app.listen(4000);
